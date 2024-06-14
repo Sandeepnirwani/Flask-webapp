a20eb97454269cfcb6e91567a00bd154ac20334f
Sandeepnirwani <141717232+Sandeepnirwani@users.noreply.github.com>
2024-06-14 17:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ from flask import flask
 app = flask(__name__)
 @app.route("/")
 def hello_world():
-  return "<p>Hello,World,Sandeep</p>
+  message "Hello WORLD hiiii!"
+  return render_template(index.html, message=message)
+  
   
